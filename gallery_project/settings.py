5136733d67283e25463cb57d057e9855f78baf66
AnnabelNkir <nkiroteannabel@gmail.com>
2021-11-28 02:06:34 +0300

    This commit adds an images model

--- gallery_project/settings.py
+++ gallery_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pyuploadcare.dj',
+    'django.contrib.staticfiles',
+    'gallery.apps.GalleryConfig'
 ]
 
 MIDDLEWARE = [
