2d517014ffae1054a785dd6d1d7df9de7cfbb988
AnnabelNkir <nkiroteannabel@gmail.com>
2021-11-27 22:25:07 +0300

    This commit adds media and static folders

--- gallery_project/settings.py
+++ gallery_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l(1&@gpntlv7vu^w(5z(za&_#)j4aw+72wm#wqbk$wokdzvijq'
+SECRET_KEY = 'django-insecure-6q!-ja(wsgr)7nyp&g(5jl*d687ez8%)zy)y3fd8#q!(dv-^om'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
