2105765b8c47f4b59548f05b437e3ac76ca9f298
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-01 12:09:52 +0300

    Deployment

--- gallery_project/settings.py
+++ gallery_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0b251791e8dc99382a35'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['catchyviews.herokuapp.com']
 
